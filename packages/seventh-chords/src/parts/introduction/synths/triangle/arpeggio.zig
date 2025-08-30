e7949ee05d0352c167b142c022689e6c70c615f9
haruki7049 <tontonkirikiri@gmail.com>
2025-08-30 11:23:12 +0900

    Update

--- packages/seventh-chords/src/parts/introduction/synths/triangle/arpeggio.zig
+++ packages/seventh-chords/src/parts/introduction/synths/triangle/arpeggio.zig
@@ -21,7 +21,7 @@ pub fn generate(allocator: std.mem.Allocator, options: Options) Wave {
             .sample_rate = options.sample_rate,
             .channels = options.channels,
             .bits = options.bits,
-        });
+        }).filter(decay);
 
         wave_list.append(wave) catch @panic("Out of memory");
     }
@@ -56,3 +56,24 @@ const Options = struct {
     channels: usize,
     bits: usize,
 };
+
+fn decay(original_wave: Wave) !Wave {
+    var result = std.ArrayList(f32).init(original_wave.allocator);
+
+    for (original_wave.data, 0..) |data, n| {
+        const i = original_wave.data.len - n;
+        const volume: f32 = @as(f32, @floatFromInt(i)) * (1.0 / @as(f32, @floatFromInt(original_wave.data.len)));
+
+        const new_data = data * volume;
+        try result.append(new_data);
+    }
+
+    return Wave{
+        .data = try result.toOwnedSlice(),
+        .allocator = original_wave.allocator,
+
+        .sample_rate = original_wave.sample_rate,
+        .channels = original_wave.channels,
+        .bits = original_wave.bits,
+    };
+}
