e286a1c6b2e5d1c3ecf5f09d994e7280919e0663
haruki7049 <tontonkirikiri@gmail.com>
2025-12-30 13:49:01 +0900

    Update

--- packages/drumming-uhouho/build.zig
+++ packages/drumming-uhouho/build.zig
@@ -1,68 +1,77 @@
 const std = @import("std");
+const lightmix = @import("lightmix");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+pub fn build(b: *std.Build) !void {
+    // `zig build` code block
+    {
+        const Root = @import("src/root.zig");
+        const Utils = @import("src/utils.zig");
+        const wave: lightmix.Wave = Root.generate(b.allocator, .{
+            .utils = Utils,
 
-    // Dependencies
-    const lightmix = b.dependency("lightmix", .{ .with_debug_features = true });
+            .bpm = 170,
+            .amplitude = 1.0,
 
-    // Modules
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    lib_mod.addImport("lightmix", lightmix.module("lightmix"));
+            .sample_rate = 44100,
+            .channels = 1,
+        }).filter(normalize);
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe_mod.addImport("lightmix", lightmix.module("lightmix"));
-    exe_mod.addImport("drumming-uhouho", lib_mod);
+        const wave_install_file: *std.Build.Step.InstallFile = try lightmix.addWaveInstallFile(b, wave, .{
+            .wave = .{ .name = "result.wav", .bit_type = .i16 },
+            .path = .{ .custom = "share" },
+        });
 
-    // Install
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "drumming-uhouho",
-        .root_module = lib_mod,
-    });
-    lib.linkLibC();
-    lib.linkSystemLibrary("portaudio-2.0");
-    lib.linkSystemLibrary("sndfile");
-    b.installArtifact(lib);
-
-    const exe = b.addExecutable(.{
-        .name = "drumming-uhouho",
-        .root_module = exe_mod,
-    });
-    b.installArtifact(exe);
-
-    // Run cmd
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        b.default_step = &wave_install_file.step;
     }
 
-    // Run step
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // `zig build test` code block
+    {
+        const target = b.standardTargetOptions(.{});
+        const optimize = b.standardOptimizeOption(.{});
 
-    // Unit tests
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+        // Dependencies
+        const lightmix_dep = b.dependency("lightmix", .{ .with_debug_features = true });
 
-    // Test step
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+        // Modules
+        const lib_mod = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        lib_mod.addImport("lightmix", lightmix_dep.module("lightmix"));
+
+        // Unit tests
+        const lib_unit_tests = b.addTest(.{ .root_module = lib_mod });
+        const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+        // Test step
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_lib_unit_tests.step);
+    }
+}
+
+fn normalize(original_wave: lightmix.Wave) !lightmix.Wave {
+    const allocator = original_wave.allocator;
+    var result: std.array_list.Aligned(f32, null) = .empty;
+
+    var max_volume: f32 = 0.0;
+    for (original_wave.data) |sample| {
+        if (@abs(sample) > max_volume)
+            max_volume = @abs(sample);
+    }
+
+    for (original_wave.data) |sample| {
+        const volume: f32 = 1.0 / max_volume;
+
+        const new_sample: f32 = sample * volume;
+        try result.append(allocator, new_sample);
+    }
+
+    return lightmix.Wave{
+        .data = try result.toOwnedSlice(allocator),
+        .allocator = allocator,
+
+        .sample_rate = original_wave.sample_rate,
+        .channels = original_wave.channels,
+    };
 }
