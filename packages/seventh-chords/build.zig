16011d375706b35a4908fd576c79e230057373d3
haruki7049 <tontonkirikiri@gmail.com>
2026-01-01 14:43:05 +0900

    Update

--- packages/seventh-chords/build.zig
+++ packages/seventh-chords/build.zig
@@ -1,68 +1,92 @@
 const std = @import("std");
+const lightmix = @import("lightmix");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+const bit_type = .i16;
 
-    // Dependencies
-    const lightmix = b.dependency("lightmix", .{ .with_debug_features = true });
+pub fn build(b: *std.Build) !void {
+    // `zig build` & `zig build play` code block
+    {
+        const Root = @import("src/root.zig");
+        const Scale = @import("src/scale.zig");
+        const Synths = @import("src/synths.zig");
+        const Generators = @import("src/generators.zig");
 
-    // Modules
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    lib_mod.addImport("lightmix", lightmix.module("lightmix"));
+        const wave: lightmix.Wave = Root.generate(b.allocator, .{
+            .scale = Scale,
+            .synths = Synths,
+            .generators = Generators,
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe_mod.addImport("lightmix", lightmix.module("lightmix"));
-    exe_mod.addImport("seventh-chords", lib_mod);
+            .bpm = 125,
+            .amplitude = 1.0,
 
-    // Install
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "seventh-chords",
-        .root_module = lib_mod,
-    });
-    lib.linkLibC();
-    lib.linkSystemLibrary("portaudio-2.0");
-    lib.linkSystemLibrary("sndfile");
-    b.installArtifact(lib);
+            .sample_rate = 44100,
+            .channels = 1,
+        }).filter(normalize);
+        defer wave.deinit();
 
-    const exe = b.addExecutable(.{
-        .name = "seventh-chords",
-        .root_module = exe_mod,
-    });
-    b.installArtifact(exe);
+        const wave_install_file: *std.Build.Step.InstallFile = try lightmix.addWaveInstallFile(b, wave, .{
+            .wave = .{ .name = "result.wav", .bit_type = bit_type },
+            .path = .{ .custom = "share" },
+        });
 
-    // Run cmd
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        const play_step = try lightmix.addDebugPlayStep(b, wave, .{
+            .step = .{ .name = "play" },
+            .wave = .{ .name = "result.wav", .bit_type = bit_type },
+            .command = &[_][]const u8{"play"}, // pkgs.sox
+        });
+
+        play_step.dependOn(&wave_install_file.step);
+        b.getInstallStep().dependOn(&wave_install_file.step);
     }
 
-    // Run step
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // `zig build test` code block
+    {
+        const target = b.standardTargetOptions(.{});
+        const optimize = b.standardOptimizeOption(.{});
 
-    // Unit tests
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+        // Dependencies
+        const lightmix_dep = b.dependency("lightmix", .{ .with_debug_features = true });
 
-    // Test step
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+        // Modules
+        const lib_mod = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        lib_mod.addImport("lightmix", lightmix_dep.module("lightmix"));
+
+        // Unit tests
+        const lib_unit_tests = b.addTest(.{ .root_module = lib_mod });
+        const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+        // Test step
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_lib_unit_tests.step);
+    }
+}
+
+fn normalize(original_wave: lightmix.Wave) !lightmix.Wave {
+    const allocator = original_wave.allocator;
+    var result: std.array_list.Aligned(f32, null) = .empty;
+
+    var max_volume: f32 = 0.0;
+    for (original_wave.data) |sample| {
+        if (@abs(sample) > max_volume)
+            max_volume = @abs(sample);
+    }
+
+    for (original_wave.data) |sample| {
+        const volume: f32 = 1.0 / max_volume;
+
+        const new_sample: f32 = sample * volume;
+        try result.append(allocator, new_sample);
+    }
+
+    return lightmix.Wave{
+        .data = try result.toOwnedSlice(allocator),
+        .allocator = allocator,
+
+        .sample_rate = original_wave.sample_rate,
+        .channels = original_wave.channels,
+    };
 }
